8a6f5cf0e6dd57b836a338219397fc213db7c53a
rodionoff <rodionov134@gmail.com>
2018-04-04 18:50:59 +0300

    comments at admin

--- messages_for_friends/admin.py
+++ messages_for_friends/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import MeMessages, FriendsMessages
 
-admin.site.register(MeMessages)
-admin.site.register(FriendsMessages)
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class MeMessageAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    search_fields = ['name']
+
+class FriendsMessagesAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(MeMessages, MeMessagesAdmin)
+admin.site.register(FriendsMessages, FriendsMessagesAdmin)
