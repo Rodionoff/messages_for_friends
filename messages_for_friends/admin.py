507f626cdd6f3280e6447301a143bedfdcf64a1e
rodionoff <rodionov134@gmail.com>
2018-04-04 19:05:20 +0300

    admins panel changes

--- messages_for_friends/admin.py
+++ messages_for_friends/admin.py
@@ -1,21 +1,16 @@
 from django.contrib import admin
-from .models import MeMessages, FriendsMessages
+from .models import MeMessages, FriendsMessages, Comment
 
 
 
 class CommentInline(admin.TabularInline):
     model = Comment
 
-class MeMessageAdmin(admin.ModelAdmin):
-    inlines = [
-        CommentInline,
-    ]
-    search_fields = ['name']
-
 class FriendsMessagesAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline,
     ]
 
-admin.site.register(MeMessages, MeMessagesAdmin)
+admin.site.register(MeMessages)
 admin.site.register(FriendsMessages, FriendsMessagesAdmin)
+admin.site.register(Comment)
